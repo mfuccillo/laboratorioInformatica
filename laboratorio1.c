e54d674ae87e6fb7f033e3b9de258aa3c985d7ca
massimo <massimo.fuccillo@numerocivico.it>
2018-10-08 14:26:46 +0200

    secondo commit

---
+++ laboratorio1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int esercizio1()
+    /* testo dell'esercizio 1 */
+    {
+    return 1;
+    }
+
+int esercizio2()
+    /* testo dell'esercizio 2 */
+    {
+    return 2;
+    }
+
+int main()
+    /* esercizi del laboratorio di informatica del politecnico 2018 */
+    {
+    int i;
+    int x1, x2;
+
+
+    x1 = esercizio1();
+    printf("il risultato dell'esercizio 1 e' %d1\n", x1);
+
+    x2 = esercizio2();
+    printf("il risultato dell'esercizio 2 e' %d\n", x1);
+
+    return 0;
+    }
+
